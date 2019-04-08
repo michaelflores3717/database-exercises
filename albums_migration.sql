c8ad18ea1f5a3c24f606e823c4af23ed19805814
Mike Flores <mike.flores3717@gmail.com>
2019-04-08 14:21:01 -0500

    commit

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  artist VARCHAR(100) DEFAULT 'NONE',
+  name VARCHAR(100) DEFAULT 'NONE',
+  release_date INT NOT NULL,
+  sales FLOAT NOT NULL,
+  genre VARCHAR(100) DEFAULT 'NONE',
+  PRIMARY KEY (id)
+);
