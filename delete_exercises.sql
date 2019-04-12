123c9878036408f1c77b8a294408108297135d9f
Mike Flores <mike.flores3717@gmail.com>
2019-04-12 16:48:31 -0500

    adding delete exercise

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,5 +1,10 @@
 USE codeup_test_db;
 
-DELETE * FROM albums WHERE release_date > 1991;
-DELETE * FROM albums WHERE genre = 'disco';
-DELETE * FROM albums WHERE artist = 'Whitney Houston';
+SELECT * FROM albums WHERE release_year > 1991;
+DELETE FROM albums WHERE release_year > 1991;
+
+SELECT * FROM albums WHERE genre = 'disco';
+DELETE FROM albums WHERE genre = 'disco';
+
+SELECT * FROM albums WHERE artist = 'Dire Straits';
+DELETE FROM albums WHERE artist = 'Dire Straits';
